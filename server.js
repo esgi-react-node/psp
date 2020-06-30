7eb477370c9e7f3099ef03df198d7db9656e7f17
lucas <xz351@hotmail.fr>
2020-06-30 19:26:33 +0200

    Updated response process

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const axios = require('axios');
 const app = express();
+const paymentAPIUrl = 'http://server:3000';
 
 app.use(express.json());
 
+
 app.get('/', function (req, res) {
-  setTimeout(() => res.sendStatus(200), 10000);
+  res.sendStatus(200);
+  setTimeout(() => {
+    axios.put(`${paymentAPIUrl}/transactions/${req.body.transactionId}/confirm`);
+  }, 10000);
 })
 
 app.listen(3000, () => console.log("listening..."));
